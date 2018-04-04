a20644d5c5356f3485fb4ac61ec1fcae5bf0e7fe
PeterYCZ <yecongzhoupeter@qq.com>
2018-04-04 09:35:36 +0800

    add the funcition of setting parameter

--- train.py
+++ train.py
@@ -6,9 +6,10 @@ import numpy as np
 import tensorflow as tf
 from conf.Parameter import Parameter
 
-def main():
+def main(argv):
     parameter = Parameter()
     print(parameter)
+    print(argv)
 
 if __name__ == "__main__":
     tf.app.run(main=main)
